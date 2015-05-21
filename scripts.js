ba77c31d7675eaf93a576f83324dd5a51489e089
Mila M <wanntofly8@gmail.com>
2015-05-20 22:24:05 -0400

    playing around with lists

--- scripts.js
+++ scripts.js
@@ -392,10 +392,8 @@ function List() {
   this.pos = 0;
   this.dataStore = []; 
   this.find = find;
-  this.toString = toString;
-  this.insert = insert; 
+  this.toString = toString; 
   this.append = append; 
-  this.remove = remove; 
   this.front = front; 
   this.end = end;
   this.prev = prev;
@@ -405,11 +403,18 @@ function List() {
   this.moveTo = moveTo; 
   this.getElement = getElement; 
   this.length = length; 
-  this.contains = contains;
+}
+
+function append(element) { 
+  this.dataStore[this.listSize++] = element;
+}
+
+function length() { 
+  return this.listSize;
 }
 
 function front() { 
-  this.pos = 0;
+  return this.pos = 0;
 }
 
 function end() {
@@ -422,9 +427,9 @@ function prev() {
 }
 
 function next() {
-  if (this.pos < this.listSize-1) {
-  ++this.pos; 
-  }
+
+  return ++this.pos; 
+ 
 }
 
 function currPos() { 
@@ -441,39 +446,182 @@ function getElement() {
 
 function displayList(list) {
   for (list.front(); list.currPos() < list.length(); list.next()) {
-    if (list.getElement() instanceof Customer) { print(list.getElement()["name"] + ", " +
-      list.getElement()["movie"]);
-    }
-   else {
-      print(list.getElement());
+    if ((list.getElement() instanceof Person) && (list.getElement()["gender"] == "male")){ 
+      console.log(list.getElement()["name"] + ", " +
+      list.getElement()["gender"]);;
     }
   }
 }
 
-var Person = function {
+
+
+var Person = function(name, gender) {
   this.name = name;
   this.gender = gender;
 }
 
-var person = new Person();
-person.name("fdg");
-person.gender("male");
-person.name("trytry");
-person.gender("female");
-person.name("retet");
-person.gender("male");
-person.name("dfhgfh");
-person.gender("male");
-person.name("uyiy");
-person.gender("female");
-person.name("wsdqws");
-person.gender("male");
-person.name("wqewe");
-person.gender("male");
-person.name("uioiupoi");
-person.gender("female");
-person.name("fertdg");
-person.gender("female");
+
+var person = new Person("emli", "female");
+var person2 = new Person("fdsmli", "male");
+var person3 = new Person("henry", "male");
+var person4 = new Person("koss", "male");
+var person5 = new Person("amelia", "female");
+var person6 = new Person("chai", "male");
+var person7 = new Person("phone", "male");
+var person8 = new Person("desk", "female");
+var person9 = new Person("top", "male");
+var person10 = new Person("tada", "female");
+
+
+
+
+var people = new List();
+people.append(person);
+people.append(person2);
+people.append(person3);
+people.append(person4);
+people.append(person5);
+people.append(person6);
+people.append(person7);
+people.append(person8);
+people.append(person9);
+people.append(person10);
+
+
+displayList(people);
+
+// 4
+
+
+function List() {
+  this.listSize = 0;
+  this.pos = 0;
+  this.dataStore = []; 
+  this.find = find;
+  this.toString = toString; 
+  this.append = append; 
+  this.front = front; 
+  this.end = end;
+  this.prev = prev;
+  this.next = next;
+  this.length = length; 
+  this.currPos = currPos;
+  this.remove = remove;
+  this.moveTo = moveTo; 
+  this.getElement = getElement; 
+  this.length = length;
+  this.contains = contains;
+}
+
+function append(element) { 
+  this.dataStore[this.listSize++] = element;
+}
+
+function length() { 
+  return this.listSize;
+}
+
+function front() { 
+  return this.pos = 0;
+}
+
+function end() {
+ this.pos = this.listSize-1;
+}
+
+function prev() {
+  if (this.pos > 0) {
+  --this.pos; }
+}
+
+function next() {
+
+  return ++this.pos; 
+ 
+}
+
+function currPos() { 
+  return this.pos;
+}
+
+function contains(element) {
+ for (var i = 0; i < this.dataStore.length; ++i) {
+ if (this.dataStore[i] == element) { 
+   return true;
+   } 
+ }
+return false; }
+
+function moveTo(position) { 
+  this.pos = position;
+}
+
+function getElement() {
+  return this.dataStore[this.pos];
+}
+
+function find(element) {
+for (var i = 0; i < this.dataStore.length; ++i) {
+if (this.dataStore[i] == element) { return i;
+} }
+return -1; }
+
+
+function remove(element) {
+  var foundAt = this.find(element); if (foundAt > -1) {
+    this.dataStore.splice(foundAt,1); --this.listSize;
+    return true;
+  }
+  return false; 
+}
+
+function displayList(list) {
+  for (list.front(); list.currPos() < list.length(); list.next()) {
+
+      console.log(list.getElement()["movie"]);
+    
+  }
+}
+
+
+
+var Customer = function(name, movie) {
+  this.name = name;
+  this.movie = movie;
+}
+
+
+function checkOut(name, movie, filmList, rentList, customerList) { 
+  if (filmList.contains(movie)) {
+  var c = new Customer(name, movie); 
+    customerList.append(c); 
+    filmList.remove(movie);
+    rentList.append(movie);
+}
+else {
+          console.log(movie + " is not available.");
+       }
+}
+
+var movies = ["movie1", "movie2"];
+var filmList = new List();
+for (var i = 0; i < movies.length; ++i) {
+       filmList.append(movies[i]);
+    }
+var customers = new List();
+var rentList = new List();
+
+checkOut("ghhgj", "movie1", filmList, rentList, customers);
+
+displayList(rentList);
+
+
+
+
+
+
+
+
 
 
 
