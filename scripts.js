e0e93e5c1af68abdf3ae57a23b7e7df4ca5be7ad
Milena Maltsev <milena@cricketscircle.com>
2015-08-13 17:59:36 -0400

    Working with sets

--- scripts.js
+++ scripts.js
@@ -1,319 +1,265 @@
-// ARRAYS
+// SETS
+// #1
 
-// Adding, removing elements to the array 
-
-var nums = [1,2,3,4,5]
-nums.push(6); // OR
-nums[nums.length] = 6;
-print(nums); // 1,2,3,4,5,6
-
-// removing
-
-var nums = [2,3,4,5]
-var newnum = 1;
-var N = nums.length;
-for (var i = N; i >= 0; --i) {
-	nums[i] = nums[i-1];
-}
-nums[0] = newnum;
-
-
-//======== FIlTER
-
-function passing(num) { 
-	return num >= 60;
+function Set() {
+  this.dataStore = [];
+  this.add = add;
+  this.remove = remove;
+  this.size = size;
+  this.union = union;
+  this.intersect = intersect;
+  this.subset = subset;
+  this.difference = difference;
+  this.show = show;
 }
 
-var grades = []; 
-for(var i=0; i < 20; ++i) {
-  grades[i] = Math.floor(Math.random() * 101); //(between 0 and 1 multiplied by 101)
-}
-var passGrades = grades.filter(passing); 
-
-print("All grades: ");
-print(grades);
-print("Passing grades: "); 
-print(passGrades);
-
-// ======== filter with strings
-
-function afterc(str) {
-  if (str.indexOf("cie") > -1) { // gives index == [] (positioning) of...
-    return true; 
-  }
-  return false; 
-}
-
-var words = ["recieve","deceive","percieve","deceit","concieve"]; 
-var misspelled = words.filter(afterc);
-print(misspelled); 
-
-// ======== two dimensional arrays
-
-Array.matrix = function(numrows, numcols, initial) { // Array.matrix sets numbeer of rows and columns and initial is the value of all elems in the array
-	var arr = []; 
-	for(var i=0; i < numrows; ++i) {
-    var columns = []; 
-	  for(var j=0; j < numcols; ++j){
-	    columns[j] = initial;
-	  }
-    arr[i] = columns;
-  }
-  return arr; 
-}
-
-var nums = Array.matrix(5,5,0); 
-print(nums[1][1]); // prints 0
-var names = Array.matrix(3,3,""); 
-names[1][2] = "Joe"; 
-print(names[1][2]); // displays "Joe"
-
-//example rows 
-
-var grades = [[89, 77, 78],[76, 82, 81],[91, 94, 89]]; // [89, 77, 78]
-                                                       // [76, 82, 81]
-                                                       // [91, 94, 89]
-var total = 0;
-var average = 0.0;
-for (var row = 0; row < grades.length; ++row) {
-	for (var col = 0; col < grades[row].length; ++col) { total += grades[row][col];
-
-	}
-  average = total / grades[row].length;
-  print("Student " + parseInt(row+1) + " average: " + average.toFixed(2)); //parseInt - parses a string argument and returns an integer of the specified radix, in this case radix is the index of an array[i+1]
-  total = 0;
-  average = 0.0;
-}
-
-
-// columns
-
-var grades = [[89, 77, 78],[76, 82, 81],[91, 94, 89]]; 
-var total = 0;
-var average = 0.0;
-for (var col = 0; col < grades.length; ++col) {
-	for (var row = 0; row < grades[col].length; ++row) { total += grades[row][col];
-	}
-	average = total / grades[col].length;
-	print("Test " + parseInt(col+1) + " average: " + average.toFixed(2));
-	total = 0;
-	average = 0.0;
-}
-
-// jagged arrays 
-
-var grades = [[89, 77],[76, 82, 81],[91, 94, 89, 99]]; 
-var total = 0;
-var average = 0.0;
-for (var row = 0; row < grades.length; ++row) {
-  for (var col = 0; col < grades[row].length; ++col) { total += grades[row][col];
-  }
-  average = total / grades[row].length;
-  print("Student " + parseInt(row+1) + " average: " + average.toFixed(2));
-  total = 0;
-  average = 0.0;
-}
-
-// Array of Objects 
-
-function Point(x,y) { 
-	this.x = x;
-  this.y = y; 
-}
-
-function displayPts(arr) {
-  for (var i = 0; i < arr.length; ++i) {
-	  print(arr[i].x + ", " + arr[i].y);
+function add(data) {
+  if (this.dataStore.indexOf(data) < 0) {
+    this.dataStore.push(data);
+    return true;
+  } else {
+    return false;
   }
 }
-var p1 = new Point(1,2);
-var p2 = new Point(3,5);
-var p3 = new Point(2,8);
-var p4 = new Point(4,4);
-var points = [p1,p2,p3,p4];
-for (var i = 0; i < points.length; ++i) {
-  print("Point " + parseInt(i+1) + ": " + points[i].x + ", " + points[i].y);
-}
 
-// ----
+function remove(data) {
+  var pos = this.dataStore.indexOf(data);
+  if(pos>-1){
+    this.dataStore.splice(pos,1);
+    return true;
+  } else {
 
-function weekTemps() { 
-	this.dataStore = []; 
-	this.add = add; 
-	this.average = average;
-}
-
-function add(temp) { 
-	this.dataStore.push(temp);
-}
-
-function average() {
-var total = 0;
-for (var i = 0; i < this.dataStore.length; ++i) {
-  total += this.dataStore[i]; 
-}
-  return total / this.dataStore.length; 
-}
-
-// exercises 
-// 1
-function Grades() { 
-	this.dataStore = []; 
-	this.add = add; 
-	this.average = average;
-}
-
-function add(grade) {
-	this.dataStore.push(grade);
-}
-
-function average() {
-	var total = 0;
-	for(var i = 0; i < this.dataStorage.length; ++i) {
-		total += this.dataStorage[i];
-	}
-
-	return total / this.DataStore.length;
-}
-
-// 2
-
-var arr = ['start', 'sdfd', 'erfter', 'ertre', 'end'];
-
-print(arr);
-
-var n = arr.length;
-for (var i = n; i >= 0; --i) { 
-	arr[i] = arr[i-1]; 
-	print(arr[i-1]);
-} 
-
-// 3
-// TODO: write this in the prototypal pattern
-
-window.dataStore = new Array.matrix(4, 7, null);
-function weekTemps() { 
-  
-  this.add = add; 
-  this.average = average;
-
-} 
-
-function add(temp, week, dayoftheweek) {
-  window.dataStore[week][dayoftheweek] = temp;
-}
-
-weekTemps.prototype.average = function() {
-  var weektotal = 0;
-	  for (var i = 0; i < window.dataStore.length; ++i) {
-	  weektotal += window.dataStore[i]; 
+    return false;
   }
-  return weektotal / window.dataStore.length; 
-}
-var stat = new weekTemps();
-stat.add(53, 0, 2);
-stat.add(43, 1, 0);
-stat.add(73, 2, 1);
-
-// stat.add(temp, week, dayoftheweek);
-// [[],
-//  [],
-//  [],
-//  []]
-
-console.log(this.dataStore);
-
-// 4
-
-function Words() { 
-  this.dataStore = []; 
-  this.add = add; 
-  this.displayWord = displayWord;
 }
 
-function add(letter) {
-  this.dataStore.push(letter);
+function show() {
+  return this.dataStore.sort();
 }
 
-function displayWord() {
-
-  var words = this.dataStore.join("");
-  console.log(words);
-}
-
-var randWord = new Words();
-randWord.add('i');
-randWord.add('p');
-randWord.add('h');
-randWord.add('o');
-randWord.add('n');
-randWord.add('e');
-
-randWord.displayWord();
-
-
-
-
-
-function weekTemps() { 
-  this.dataStore = function() {
-    Array.prototype.matrix(4, 7, null) // trying to copy an Array object into weekTemps
-  };
-
-} 
-
-
-
-weekTemps.prototype.add = function(temp, week, dayoftheweek) {
-  this.dataStore[week][dayoftheweek] = temp;
-}
-
-weekTemps.prototype.average = function() {
-  var weektotal = 0;
-    for (var i = 0; i < this.dataStore.length; ++i) {
-    weektotal += this.dataStore[i]; 
+function contains(data) {
+  if (this.dataStore.indexOf(data) > -1) {
+   return true;
+  } else {
+    return false;
   }
-  return weektotal / this.dataStore.length; 
 }
-var stat = new weekTemps();
-stat.add(53, 0, 2);
-stat.add(43, 1, 0);
-stat.add(73, 2, 1);
 
-// stat.add(temp, week, dayoftheweek);
-// [[],
-//  [],
-//  [],
-//  []]
-
-console.log(this.dataStore);
-
-
-// Don't polute the global namespace!
-// TODO: show me the version of code you have in mind
-// TODO: create two "classes" that share some prototyped behavior
-
-var Foo = function() {
-    this.instMeth = 3;
-    this.add = function(val) {
-        return (this.instMeth + val);
+function union(set) {
+  var tempSet = new Set();
+  for (var i = 0; i < this.dataStore.length; ++i) {
+    tempSet.add(this.dataStore[i]);
+  }
+  for (var i = 0; i < set.dataStore.length; ++i) {
+    if (!tempSet.contains(set.dataStore[i])) {
+       tempSet.dataStore.push(set.dataStore[i]);
     }
+  }
+  return tempSet;
+}
+
+function intersect(set) {
+  var tempSet = new Set();
+  for (var i = 0; i < this.dataStore.length; ++i) {
+    if (set.contains(this.dataStore[i])) {
+      tempSet.add(this.dataStore[i]);
+    }
+  }
+  return tempSet;
+}
+
+function subset(set) {
+  if (this.size() > set.size()) {
+    return false; }
+  else {
+    for each (var member in this.dataStore) {
+      if (!set.contains(member)) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
+
+function size() {
+  return this.dataStore.length;
+}
+
+function difference(set) {
+  var tempSet = new Set();
+  for (var i = 0; i < this.dataStore.length; ++i) {
+    if (!set.contains(this.dataStore[i])) {
+      tempSet.add(this.dataStore[i]);
+    }
+  }
+  return tempSet;
 }
 
 
-var Bar = function() {
-    this.diffMeth = 5;
-    this.add = function(val) {
-        return (this.diffMeth + val + 4);
-    }   
+
+var names = new Set();
+names.add("David");
+names.add("Jennifer");
+names.add("Cynthia");
+names.add("Mike");
+names.add("Raymond");
+
+names.show();
+
+// #2
+
+function Set() {
+  this.dataStore = new LList();
+  this.add = add;
+  this.remove = remove;
+  this.size = 0;
+  this.union = union;
+  this.intersect = intersect;
+  this.subset = subset;
+  this.difference = difference;
+  this.show = show;
 }
 
-var f = new Foo()
-f.instMeth;
+function add(data) {
+    var element = this.dataStore.head.element;
+  //if (this.head != null) {
+    this.dataStore.insert(data, element);
+}
 
-var b = new Bar();
-b.diffMeth;
+function remove(data) {
+  var pos = this.dataStore.indexOf(data);
+  if(pos>-1){
+    this.dataStore.splice(pos,1);
+    return true;
+  } else {
+
+    return false;
+  }
+}
+
+function show() {
+  return this.dataStore;
+}
+
+function contains(data) {
+  if (this.dataStore.indexOf(data) > -1) {
+   return true;
+  } else {
+    return false;
+  }
+}
+
+function union(set) {
+  var tempSet = new Set();
+  for (var i = 0; i < this.dataStore.length; ++i) {
+    tempSet.add(this.dataStore[i]);
+  }
+  for (var i = 0; i < set.dataStore.length; ++i) {
+    if (!tempSet.contains(set.dataStore[i])) {
+       tempSet.dataStore.push(set.dataStore[i]);
+    }
+  }
+  return tempSet;
+}
+
+function intersect(set) {
+  var tempSet = new Set();
+  for (var i = 0; i < this.dataStore.length; ++i) {
+    if (set.contains(this.dataStore[i])) {
+      tempSet.add(this.dataStore[i]);
+    }
+  }
+  return tempSet;
+}
+
+function subset(set) {
+  if (this.size() > set.size()) {
+    return false; }
+  else {
+    for each (var member in this.dataStore) {
+      if (!set.contains(member)) {
+        return false;
+      }
+    }
+  }
+  return true;
+}
 
 
+function difference(set) {
+  var tempSet = new Set();
+  for (var i = 0; i < this.dataStore.length; ++i) {
+    if (!set.contains(this.dataStore[i])) {
+      tempSet.add(this.dataStore[i]);
+    }
+  }
+  return tempSet;
+}
+
+function Node(element) {
+  this.element = element;
+  this.next = null;
+}
 
 
+function LList() {
+  this.head = new Node("head");
+  this.find = find;
+  this.insert = insert;
+}
+
+
+function find(item) {
+  var currNode = this.head;
+  while (currNode.element != item) {
+      currNode = currNode.next;
+   }
+  return currNode;
+}
+
+function insert(newElement, item) {
+  var newNode = new Node(newElement);
+  var current = this.find(item);
+  newNode.next = current.next;
+  current.next = newNode;
+}
+
+
+var names = new Set();
+names.add("David");
+names.add("Jennifer");
+names.add("Cynthia");
+names.add("Mike");
+names.add("Raymond");
+
+names.show();
+
+////////////////
+
+function add(data) {
+    var element = this.dataStore.head.element;
+  //if (this.head != null) {
+    this.dataStore.insert(data, element);
+    this.size++;
+    //return true;
+  } else {
+
+    while (this.size < 4) {
+      var cur = this.dataStore.head.next
+      var last = cur;
+      //if (cur == null) {
+        //var ele = cur.element;
+        this.dataStore.insert(data, last.element);
+        console.log(last);
+        //return cur;
+
+        this.size++;
+      //}
+    }
+
+      //var element = this.dataStore.head.next.element;
+      //this.dataStore.insert(data, element);
+      //this.size++;
+  }
+  console.log(this.dataStore.head);
+}
